85bb273dff1da481585d856169ad0ed278cc570b
YT65a <84340364+YT65a@users.noreply.github.com>
2021-09-03 08:19:38 -0500

    finished switching screens work on inputting the text into final screen

--- app/src/main/java/com/example/signupapp/FinishedScreen.java
+++ app/src/main/java/com/example/signupapp/FinishedScreen.java
@@ -5,19 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class FinishedScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_finished_screen);
     }
 
-    public void sendMessage(View v) {
+    public void startOver(View v) {
         // creating the ability to switch screens (addressing the envelope)
-        Intent switchIntent = new Intent(this, SignUpScreen.class);
+        Intent switchIntent = new Intent(this, MainActivity.class);
         startActivity(switchIntent);
     }
 }
--- app/src/main/java/com/example/signupapp/SignUpScreen.java
+++ app/src/main/java/com/example/signupapp/SignUpScreen.java
@@ -1,4 +1,34 @@
 package com.example.signupapp;
 
-public class SignUpScreen {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class SignUpScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_screen);
+    }
+
+    public void sendMessage(View v) {
+        // creating the ability to switch screens (addressing the envelope)
+        Intent switchIntent = new Intent(this, FinishedScreen.class);
+        startActivity(switchIntent);
+    }
+
+    public void goHome(View v) {
+        // creating the ability to switch screens (addressing the envelope)
+        Intent switchIntent = new Intent(this, MainActivity.class);
+        startActivity(switchIntent);
+    }
+    public void submit(View v) {
+        // creating the ability to switch screens (addressing the envelope)
+        Intent switchIntent = new Intent(this, FinishedScreen.class);
+        startActivity(switchIntent);
+    }
 }
+
